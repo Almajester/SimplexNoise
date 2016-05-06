782570f6f5f2e2af5d20b27db1f7b5c8b51957ba
labadmin <labadmin@mcs008.local>
2016-05-06 17:30:03 -0400

    Added SimplexTables with perm[] and helper dot() functions. Code still not operational.

--- SimplexNoise.py
+++ SimplexNoise.py
@@ -42,6 +42,11 @@ noise value  at each frame. By adding a second parameter on the second
 dimension, you can ensure that each gets a unique noise value and they don't
 all look identical.
 """
+
+from math import sqrt
+from SimplexTables import *
+
+
 class SimplexNoise(object):
 
     @staticmethod
@@ -218,84 +223,92 @@ class SimplexNoise(object):
         """
         return SimplexNoise.raw_noise_4d(x, y, z, w)*(hiBound - loBound)/2 + (hiBound + loBound)/2
 
-    /**
-     * raw_noise_2d() -- 2D raw Simplex noise
-     * @return value will be between [-1, 1]
-     */
-    public static float raw_noise_2d(float x, float y) {
-        // Noise contributions from the three corners
-        float n0, n1, n2;
 
-        // Skew the input space to determine which simplex cell we re in
-        float F2 = 0.5f * ((float) Math.sqrt(3.0) - 1.0f);
-        // Hairy factor for 2D
-        float s = (x + y) * F2;
-        int i = fastfloor( x + s );
-        int j = fastfloor( y + s );
+    @staticmethod
+    def raw_noise_2d(x, y):
+        """
+        raw_noise_2d() -- 2D raw Simplex noise
+        :param x:
+        :param y:
+        :return: value will be between [-1, 1]
+        """
+        # Noise contributions from the three corners
+        n0, n1, n2 = 0.0, 0.0, 0.0
 
-        float G2 = (3.0f - (float) Math.sqrt(3.0)) / 6.0f;
-        float t = (i + j) * G2;
-        // Unskew the cell origin back to (x,y) space
-        float X0 = i-t;
-        float Y0 = j-t;
-        // The x,y distances from the cell origin
-        float x0 = x-X0;
-        float y0 = y-Y0;
+        # Skew the input space to determine which simplex cell we're in
+        F2 = 0.5*(sqrt(3.0) - 1.0)
+        # Hairy factor for 2D
+        s = (x + y) * F2
+        i = fastfloor( x + s )
+        j = fastfloor( y + s )
 
-        // For the 2D case, the simplex shape is an equilateral triangle.
-        // Determine which simplex we are in.
-        int i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords
-        if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1)
-        else {i1=0; j1=1;} // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+        G2 = (3.0 - sqrt(3.0))/6.0
+        t = (i + j)*G2
+        # Unskew the cell origin back to (x,y) space
+        X0 = i-t
+        Y0 = j-t
+        # The x,y distances from the cell origin
+        x0 = x-X0
+        y0 = y-Y0
 
-        // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
-        // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
-        // c = (3-sqrt(3))/6
-        float x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
-        float y1 = y0 - j1 + G2;
-        float x2 = x0 - 1.0f + 2.0f * G2; // Offsets for last corner in (x,y) unskewed coords
-        float y2 = y0 - 1.0f + 2.0f * G2;
+        # For the 2D case, the simplex shape is an equilateral triangle.
+        # Determine which simplex we are in.
+        # Offsets for second (middle) corner of simplex in (i,j) coords
+        if x0>y0:  # lower triangle, XY order: (0,0)->(1,0)->(1,1)
+            i1=1
+            j1=0
+        else:  # upper triangle, YX order: (0,0)->(0,1)->(1,1)
+            i1=0
+            j1=1
 
-        // Work out the hashed gradient indices of the three simplex corners
-        int ii = i & 255;
-        int jj = j & 255;
-        int gi0 = perm[ii+perm[jj]] % 12;
-        int gi1 = perm[ii+i1+perm[jj+j1]] % 12;
-        int gi2 = perm[ii+1+perm[jj+1]] % 12;
+        # A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
+        # a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
+        # c = (3-sqrt(3))/6
+        x1 = x0 - i1 + G2     # Offsets for middle corner in (x,y) unskewed coords
+        y1 = y0 - j1 + G2
+        x2 = x0 - 1.0 + 2.0*G2    # Offsets for last corner in (x,y) unskewed coords
+        y2 = y0 - 1.0 + 2.0*G2
+
+        # Work out the hashed gradient indices of the three simplex corners
+        ii = i & 255
+        jj = j & 255
+        gi0 = perm[ii+perm[jj]] % 12
+        gi1 = perm[ii+i1+perm[jj+j1]] % 12
+        gi2 = perm[ii+1+perm[jj+1]] % 12
 
         // Calculate the contribution from the three corners
         float t0 = 0.5f - x0*x0-y0*y0;
         if(t0<0) n0 = 0.0f;
-        else {
+        else [
             t0 *= t0;
             n0 = t0 * t0 * dot(grad3[gi0][0], grad3[gi0][1], x0, y0); // (x,y) of grad3 used for 2D gradient
-        }
+        ]
 
         float t1 = 0.5f - x1*x1-y1*y1;
         if(t1<0) n1 = 0.0f;
-        else {
+        else [
             t1 *= t1;
             n1 = t1 * t1 * dot(grad3[gi1][0], grad3[gi1][1], x1, y1);
-        }
+        ]
 
         float t2 = 0.5f - x2*x2-y2*y2;
         if(t2<0) n2 = 0.0f;
-        else {
+        else [
             t2 *= t2;
             n2 = t2 * t2 * dot(grad3[gi2][0], grad3[gi2][1], x2, y2);
-        }
+        ]
 
         // Add contributions from each corner to get the final noise value.
         // The result is scaled to return values in the interval [-1,1].
         return 70.0f * (n0 + n1 + n2);
-    }
+    ]
 
 
     /**
      * raw_noise_3d() -- 3D raw Simplex noise
      * @return value will be between [-1, 1]
      */
-    public static float raw_noise_3d(float x, float y, float z) {
+    public static float raw_noise_3d(float x, float y, float z) [
         float n0, n1, n2, n3; // Noise contributions from the four corners
 
         // Skew the input space to determine which simplex cell we re in
@@ -319,16 +332,16 @@ class SimplexNoise(object):
         int i1, j1, k1; // Offsets for second corner of simplex in (i,j,k) coords
         int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
 
-        if(x0>=y0) {
-            if(y0>=z0) { i1=1; j1=0; k1=0; i2=1; j2=1; k2=0; } // X Y Z order
-            else if(x0>=z0) { i1=1; j1=0; k1=0; i2=1; j2=0; k2=1; } // X Z Y order
-            else { i1=0; j1=0; k1=1; i2=1; j2=0; k2=1; } // Z X Y order
-        }
-        else { // x0<y0
-            if(y0<z0) { i1=0; j1=0; k1=1; i2=0; j2=1; k2=1; } // Z Y X order
-            else if(x0<z0) { i1=0; j1=1; k1=0; i2=0; j2=1; k2=1; } // Y Z X order
-            else { i1=0; j1=1; k1=0; i2=1; j2=1; k2=0; } // Y X Z order
-        }
+        if(x0>=y0) [
+            if(y0>=z0) [ i1=1; j1=0; k1=0; i2=1; j2=1; k2=0; ] // X Y Z order
+            else if(x0>=z0) [ i1=1; j1=0; k1=0; i2=1; j2=0; k2=1; ] // X Z Y order
+            else [ i1=0; j1=0; k1=1; i2=1; j2=0; k2=1; ] // Z X Y order
+        ]
+        else [ // x0<y0
+            if(y0<z0) [ i1=0; j1=0; k1=1; i2=0; j2=1; k2=1; ] // Z Y X order
+            else if(x0<z0) [ i1=0; j1=1; k1=0; i2=0; j2=1; k2=1; ] // Y Z X order
+            else [ i1=0; j1=1; k1=0; i2=1; j2=1; k2=0; ] // Y X Z order
+        ]
 
         // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
         // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
@@ -356,43 +369,43 @@ class SimplexNoise(object):
         // Calculate the contribution from the four corners
         float t0 = 0.6f - x0*x0 - y0*y0 - z0*z0;
         if(t0<0) n0 = 0.0f;
-        else {
+        else [
             t0 *= t0;
             n0 = t0 * t0 * dot(grad3[gi0][0], grad3[gi0][1], grad3[gi0][2], x0, y0, z0);
-        }
+        ]
 
         float t1 = 0.6f - x1*x1 - y1*y1 - z1*z1;
         if(t1<0) n1 = 0.0f;
-        else {
+        else [
             t1 *= t1;
             n1 = t1 * t1 * dot(grad3[gi1][0], grad3[gi1][1], grad3[gi1][2], x1, y1, z1);
-        }
+        ]
 
         float t2 = 0.6f - x2*x2 - y2*y2 - z2*z2;
         if(t2<0) n2 = 0.0f;
-        else {
+        else [
             t2 *= t2;
             n2 = t2 * t2 * dot(grad3[gi2][0], grad3[gi2][1], grad3[gi2][2], x2, y2, z2);
-        }
+        ]
 
         float t3 = 0.6f - x3*x3 - y3*y3 - z3*z3;
         if(t3<0) n3 = 0.0f;
-        else {
+        else [
             t3 *= t3;
             n3 = t3 * t3 * dot(grad3[gi3][0], grad3[gi3][1], grad3[gi3][2], x3, y3, z3);
-        }
+        ]
 
         // Add contributions from each corner to get the final noise value.
         // The result is scaled to stay just inside [-1,1]
         return 32.0f*(n0 + n1 + n2 + n3);
-    }
+    ]
 
 
     /**
      * raw_noise_4d() -- 4D raw Simplex noise
      * @return value will be between [-1, 1]
      */
-    public static float raw_noise_4d(float x, float y, float z, float w ) {
+    public static float raw_noise_4d(float x, float y, float z, float w ) [
         // The skewing and unskewing factors are hairy again for the 4D case
         float F4 = ((float) Math.sqrt(5.0) - 1.0f)/4.0f;
         float G4 = (5.0f - (float) Math.sqrt(5.0))/20.0f;
@@ -487,115 +500,41 @@ class SimplexNoise(object):
         // Calculate the contribution from the five corners
         float t0 = 0.6f - x0*x0 - y0*y0 - z0*z0 - w0*w0;
         if(t0<0) n0 = 0.0f;
-        else {
+        else [
             t0 *= t0;
             n0 = t0 * t0 * dot(grad4[gi0][0], grad4[gi0][1], grad4[gi0][2], grad4[gi0][3], x0, y0, z0, w0);
-        }
+        ]
 
         float t1 = 0.6f - x1*x1 - y1*y1 - z1*z1 - w1*w1;
         if(t1<0) n1 = 0.0f;
-        else {
+        else [
             t1 *= t1;
             n1 = t1 * t1 * dot(grad4[gi1][0], grad4[gi1][1], grad4[gi1][2], grad4[gi1][3], x1, y1, z1, w1);
-        }
+        ]
 
         float t2 = 0.6f - x2*x2 - y2*y2 - z2*z2 - w2*w2;
         if(t2<0) n2 = 0.0f;
-        else {
+        else [
             t2 *= t2;
             n2 = t2 * t2 * dot(grad4[gi2][0], grad4[gi2][1], grad4[gi2][2], grad4[gi2][3], x2, y2, z2, w2);
-        }
+        ]
 
         float t3 = 0.6f - x3*x3 - y3*y3 - z3*z3 - w3*w3;
         if(t3<0) n3 = 0.0f;
-        else {
+        else [
             t3 *= t3;
             n3 = t3 * t3 * dot(grad4[gi3][0], grad4[gi3][1], grad4[gi3][2], grad4[gi3][3], x3, y3, z3, w3);
-        }
+        ]
 
         float t4 = 0.6f - x4*x4 - y4*y4 - z4*z4 - w4*w4;
         if(t4<0) n4 = 0.0f;
-        else {
+        else [
             t4 *= t4;
             n4 = t4 * t4 * dot(grad4[gi4][0], grad4[gi4][1], grad4[gi4][2], grad4[gi4][3], x4, y4, z4, w4);
-        }
+        ]
 
         // Sum up and scale the result to cover the range [-1,1]
         return 27.0f * (n0 + n1 + n2 + n3 + n4);
-    }
+    ]
 
 
-    static int fastfloor(float x) {
-        return x > 0 ? (int) x : (int) x - 1;
-    }
-
-    static float dot(int gx, int gy, float x, float y) {
-        return gx*x + gy*y;
-    }
-    static float dot(int gx, int gy, int gz, float x, float y, float z) {
-        return gx*x + gy*y + gz*z;
-    }
-    static float dot(int gx, int gy, int gz, int gw, float x, float y, float z, float w) {
-        return gx*x + gy*y + gz*z + gw*w;
-    }
-
-    // The gradients are the midpoints of the vertices of a cube.
-    public static final int [][]grad3 = {
-        {1,1,0}, {-1,1,0}, {1,-1,0}, {-1,-1,0},
-        {1,0,1}, {-1,0,1}, {1,0,-1}, {-1,0,-1},
-        {0,1,1}, {0,-1,1}, {0,1,-1}, {0,-1,-1}
-    };
-
-    // The gradients are the midpoints of the vertices of a hypercube.
-    public static final int [][]grad4 = {
-        {0,1,1,1},  {0,1,1,-1},  {0,1,-1,1},  {0,1,-1,-1},
-        {0,-1,1,1}, {0,-1,1,-1}, {0,-1,-1,1}, {0,-1,-1,-1},
-        {1,0,1,1},  {1,0,1,-1},  {1,0,-1,1},  {1,0,-1,-1},
-        {-1,0,1,1}, {-1,0,1,-1}, {-1,0,-1,1}, {-1,0,-1,-1},
-        {1,1,0,1},  {1,1,0,-1},  {1,-1,0,1},  {1,-1,0,-1},
-        {-1,1,0,1}, {-1,1,0,-1}, {-1,-1,0,1}, {-1,-1,0,-1},
-        {1,1,1,0},  {1,1,-1,0},  {1,-1,1,0},  {1,-1,-1,0},
-        {-1,1,1,0}, {-1,1,-1,0}, {-1,-1,1,0}, {-1,-1,-1,0}
-    };
-
-    // Permutation table.  The same list is repeated twice.
-    public static final int []perm = {
-        151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
-        8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
-        35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168,68,175,74,165,71,
-        134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
-        55,46,245,40,244,102,143,54,65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
-        18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,
-        250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
-        189,28,42,223,183,170,213,119,248,152,2,44,154,163,70,221,153,101,155,167,43,
-        172,9,129,22,39,253,19,98,108,110,79,113,224,232,178,185,112,104,218,246,97,
-        228,251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,14,239,
-        107,49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,
-        138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180,
-        151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
-        8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
-        35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168,68,175,74,165,71,
-        134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
-        55,46,245,40,244,102,143,54,65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
-        18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,
-        250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
-        189,28,42,223,183,170,213,119,248,152,2,44,154,163,70,221,153,101,155,167,43,
-        172,9,129,22,39,253,19,98,108,110,79,113,224,232,178,185,112,104,218,246,97,
-        228,251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,14,239,
-        107,49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,
-        138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-    };
-
-    // A lookup table to traverse the simplex around a given point in 4D.
-    public static final int [][]simplex = {
-        {0,1,2,3},{0,1,3,2},{0,0,0,0},{0,2,3,1},{0,0,0,0},{0,0,0,0},{0,0,0,0},{1,2,3,0},
-        {0,2,1,3},{0,0,0,0},{0,3,1,2},{0,3,2,1},{0,0,0,0},{0,0,0,0},{0,0,0,0},{1,3,2,0},
-        {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},
-        {1,2,0,3},{0,0,0,0},{1,3,0,2},{0,0,0,0},{0,0,0,0},{0,0,0,0},{2,3,0,1},{2,3,1,0},
-        {1,0,2,3},{1,0,3,2},{0,0,0,0},{0,0,0,0},{0,0,0,0},{2,0,3,1},{0,0,0,0},{2,1,3,0},
-        {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},
-        {2,0,1,3},{0,0,0,0},{0,0,0,0},{0,0,0,0},{3,0,1,2},{3,0,2,1},{0,0,0,0},{3,1,2,0},
-        {2,1,0,3},{0,0,0,0},{0,0,0,0},{0,0,0,0},{3,1,0,2},{0,0,0,0},{3,2,0,1},{3,2,1,0}
-    };
-    
-}
