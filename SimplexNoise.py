85a0409760d44c5b313be307cca16fc2bb8143e8
labadmin <labadmin@mcs008.local>
2016-05-09 14:11:07 -0400

    Added a cos function import to SimplexNoise.py

--- SimplexNoise.py
+++ SimplexNoise.py
@@ -45,6 +45,7 @@ dimension, you can ensure that each gets a unique noise value and they don't
 all look identical.
 """
 
+from math import cos
 from math import sqrt
 
 def octave_noise_2d(octaves, persistence, scale, x, y):
